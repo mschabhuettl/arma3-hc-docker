9fd7183bc7224792a19785cc2bade1d8d0f16cd8
Dan Albert <dan@gingerhq.net>
2017-10-30 20:00:03 -0700

    Add Dockerfile for Arma 3 headless clients.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# arma3-headless-client-docker
+# Arma 3 Headless Client
+
 Docker image for Arma 3 headless clients.
+
+```bash
+docker run -it danalbert/arma3-headless-client \
+  -e STEAM_USER=$USER -e STEAM_PASS=$PASS \
+  -e ARMA_HOST=$SERVER_ADDR -e ARMA_PORT=$SERVER_PORT -e ARMA_PASS=$SERVER_PASS
+```
---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+set -x
+
+if [ -z "$STEAM_USER" ]; then
+  >&2 echo "STEAM_USER is not set"
+  exit 1
+fi
+
+if [ -z "$STEAM_PASS" ]; then
+  >&2 echo "STEAM_PASS is not set"
+  exit 1
+fi
+
+if [ -z "$ARMA_PORT" ]; then
+  ARMA_PORT=2302
+fi
+
+/usr/games/steamcmd \
+    +login $STEAM_USER $STEAM_PASS \
+    +runscript `realpath install_arma3.txt` \
+
+./arma3server \
+  -client \
+  -connect=$ARMA_HOST \
+  -port=$ARMA_PORT \
+  -password="$ARMA_PASS" \
+  -noSound \
