f59cbe8df3f204cd7589b9f5af8ed6c3f2141c23
Dan Albert <dan@gingerhq.net>
2017-11-04 12:21:50 -0700

    Create the MPMissionsCache directory.
    arma3server doesn't create the MPMissionsCache directory, so the
    client will just crash if it's not already created.

--- start.sh
+++ start.sh
@@ -20,6 +20,10 @@ fi
     +login $STEAM_USER $STEAM_PASS \
     +runscript `realpath install_arma3.txt` \
 
+# arma3server doesn't create the MPMissionsCache directory, so the client will
+# just crash if it's not already created.
+mkdir -p "/root/.local/share/Arma 3/MPMissionsCache"
+
 cd /arma3
 ./arma3server \
   -client \
